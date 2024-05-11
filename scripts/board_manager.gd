4b96e909f731b8cc4cfcc08b4c377ff8e2ccc3e5
Lincoln Ruteski <lruteski@gmail.com>
2024-05-10 23:40:13 -0300

    adicionado paredes internas(grid) aleatorias sem repetir posição

--- scripts/board_manager.gd
+++ scripts/board_manager.gd
@@ -2,19 +2,21 @@ extends Node2D
 
 @export var floor_tile: PackedScene
 @export var outer_wall_tile: PackedScene
+@export var wall_tile: PackedScene
 
 var column := 8
 var rows := 8
 var space := 32
 var grid_position := []
+var wall_count = Count.new(5,9) # quantidade de wall criados no grid - dificuldade
 
 
 func _ready() -> void:
-	initialize_list()
-	board_setup()
+	setup_scene()
 
-
-func initialize_list() -> void:
+	
+#grid
+func _initialize_list() -> void:
 	# configuracao grid
 	grid_position.clear()
 	for x in column - 1:
@@ -22,7 +24,8 @@ func initialize_list() -> void:
 			grid_position.append(Vector2(x * space, y * space))
 	
 
-func board_setup() -> void:
+#walls
+func _board_setup() -> void:
 	for x in range(-1, column + 1):
 		for y in range(-1, rows +1):
 			# jeito 1 de fazer - ternario
@@ -37,3 +40,33 @@ func board_setup() -> void:
 			
 			temp.global_position = Vector2(x * space, y * space)
 			add_child(temp)
+
+			
+func _random_position()	-> Vector2:
+	var random: int = randi_range(0, grid_position.size() - 1)
+	var random_pos = grid_position[random]
+	grid_position.erase(random)
+	return random_pos
+	
+			
+func _spawn_tile() -> void:
+	var tile_count := randi_range(wall_count.min, wall_count.max)
+	for i in tile_count:
+		var tile: Node2D = wall_tile.instantiate()
+		tile.global_position = _random_position()
+		add_child(tile)
+	
+
+func setup_scene():
+	_initialize_list()
+	_board_setup()
+	_spawn_tile()
+		
+			
+class Count:
+	var min: int
+	var max: int
+	
+	func _init(_min: int, _max: int) -> void:
+		min = _min
+		max = _max
--- scripts/manager/game_manager.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	$board_manager.setup_scene(GameController.level)
